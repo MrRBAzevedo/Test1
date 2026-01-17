99c5ae8403848034fb5dacda7631d95d5947bda1
Renan Azevedo <renan.azevedo@academico.ifrn.edu.br>
2026-01-17 20:46:34 -0300

    Soma de números

--- Pasta1/arquivo1.py
+++ Pasta1/arquivo1.py
@@ -1,2 +1,6 @@
-print('Hello World')
+import os
+s = input('Digite seu nome: ')
+print(f'Seja bem vindo, {s}!')
 
+
+os.system('cls')
---
+++ Pasta1/arquivo2.py
@@ -0,0 +1,3 @@
+x = int(input('Digite um número: '))
+y = int(input('Digite outro número: '))
+print(f'A soma de {x} e {y} é {x + y}')
