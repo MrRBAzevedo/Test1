707efc782c0824e36a206056f18a4a26151aec03
Renan Azevedo <renan.azevedo@academico.ifrn.edu.br>
2026-01-18 12:24:34 -0300

    Beecrowd problem

--- Pasta1/arquivo1.py
+++ Pasta1/arquivo1.py
@@ -1,4 +1,16 @@
-s = input('Digite seu nome: ')
-print(f'Seja bem vindo, {s}!')
+salario = float(input())
 
+if salario <= 400:
+    reajuste = 15
+elif salario <= 800:
+    reajuste = 12
+elif salario <= 1200:
+    reajuste = 10
+elif salario <= 2000:
+    reajuste = 7
+else:
+    reajuste = 4
 
+print(f'Novo salario: {salario + salario * reajuste * 0.01:.2f}')
+print(f'Reajuste ganho: {salario * reajuste * 0.01:.2f}')
+print(f'Em percentual: {reajuste} %')
