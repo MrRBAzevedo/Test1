ebded8c7f747a6378995700f938534e4ed7122a3
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-05-31 13:27:06 -0300

    Deu errado :(

--- Pasta2/arquivo1.py
+++ Pasta2/arquivo1.py
@@ -1,1 +1,18 @@
+fim = int(input())
+numero = 2
+soma = 0
 
+while numero < fim:
+    divisor = 2
+    
+    while divisor < numero:
+        if numero % divisor == 0:
+            break
+
+        divisor += 1
+    else:
+        soma += numero
+        
+    numero += 1
+    
+print(soma)
